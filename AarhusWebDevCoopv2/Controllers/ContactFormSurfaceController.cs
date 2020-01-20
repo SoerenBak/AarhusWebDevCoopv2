5644f8283f15e43124934b2abd8caf9a3f4447d9
SoerenBak <34088652+SoerenBak@users.noreply.github.com>
2020-01-20 14:01:22 +0100

    Start

---
+++ AarhusWebDevCoopv2/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,44 @@
+﻿using AarhusWebDevCoopv2.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using System.Net.Mail;
+using Umbraco.Core.Models;
+
+
+namespace AarhusWebDevCoopv2.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: ContactFormSurface
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactForm());
+        }
+
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactForm model)
+        {
+
+            if (!ModelState.IsValid) { return RedirectToCurrentUmbracoPage(); }
+
+            MailMessage message = new MailMessage();
+            message.To.Add("fff56k@gmail.com");
+            message.Subject = model.Subject;
+            message.From = new MailAddress(model.Email, model.Name);
+            message.Body = model.Message;
+
+            using (SmtpClient smtp = new SmtpClient())
+            {
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network; smtp.UseDefaultCredentials = false; smtp.EnableSsl = true; smtp.Host = "smtp.gmail.com"; smtp.Port = 587; smtp.Credentials = new System.Net.NetworkCredential("fff56k@gmail.com", "password");        // send mail    smtp.Send(message); } 
+
+            }
+
+            TempData["success"] = true;
+        }
+
+    }
+}
